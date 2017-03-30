5919879bed522d21fd0a2d2e66db8e41f5f5d431
Bernhard Brunner <bernhard.brunner@epr.ch>
2017-03-30 10:47:11 +0200

    yaml parser, more utilty functions

--- brlib.sh
+++ brlib.sh
@@ -2,14 +2,19 @@
 #Symlink to $HOME/bin or 
 # /usr/local/lib/brlib.sh and use with ". /usr/local/lib/brlib.sh"
 # Command line switch -BRDEBUG will set BRDEBUG=1
-#Last modified: 2014-02-23 19:09
-if [ -z "${brVersion-}" ]; then
+#Last modified: 2017-03-22 08:36
+if [ ! -z "${brVersion-}" ]; then
+    return 0
+fi
 brVersion="1.0"
+
+#xdbus="/home/brb/.config/Xdbus"
+#[ -e $xdbus ] && source $xdbus
 
 # It's easier than hand-coding color.
 #[ -z "$TERM" ] && TERM=dumb
 if [[ "$TERM" != "" && "$TERM" != "dumb" ]] ; then  # && ( hash tput ) ; then
-#    echo valid [$TERM]
+    #    echo valid [$TERM]
     ansibold="$(tput bold)"
     ansinormal="$(tput sgr0)"
     ansired="$ansibold$(tput setaf 1)"
@@ -26,7 +31,7 @@ if [[ "$TERM" != "" && "$TERM" != "dumb" ]] ; then  # && ( hash tput ) ; then
         echo "$(tput cols)" 
     }
 else
-#    echo Invalid [$TERM]
+    #    echo Invalid [$TERM]
     ansibold=""
     ansinormal=""
     ansired=""
@@ -40,8 +45,8 @@ else
     ansiwhite=""
     ansicolumns=80
     function ansiColumns() { 
-       echo 80 
-    }
+    echo 80 
+}
 fi
 
 # default values
@@ -50,10 +55,10 @@ BRERRORCOUNT=0
 BRERRORABORT=1
 
 # spinner for long during processes
-brspinstr=( "-" "\\" "|" "/" )
 brspinvar=0
 function brspin()
 {
+    local brspinstr=( "-" "\\" "|" "/" )
     printf "%c\b" ${brspinstr[$brspinvar]}
     let brspinvar=$((brspinvar + 1))
     let brspinvar=$((brspinvar % 4))
@@ -62,12 +67,41 @@ function brspin()
 ######################################################################
 # Environment information
 
+##! directory where the script is running
+function brBaseDir
+{
+    echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+}
+
+##! name of the script, symlinks not resolved, without path
+function brScriptName
+{
+    echo $(basename $0 .sh)
+}
+
+##! determine public ip
+function brGetPublicIP
+{
+    dig +short myip.opendns.com @resolver1.opendns.com &>/dev/null || curl -s http://whatismyip.akamai.com/
+}
+
+##! return active dns server
+function brGetDNS
+{
+    if hash nmcli ; then
+        nmcli dev show | grep DNS | head -n 1 | awk '{print $2}'
+    else
+        brWarn "(no nmcli)"
+    fi
+}
+
+##! return local ip address
 function brGetLocalIP
 {
     ip route get 8.8.8.8 | awk '{ print $NF; exit }'
 }
 
-# get DISTRIB_ID and DISTRIB_RELEASE
+##! get DISTRIB_ID and DISTRIB_RELEASE
 function brGetDistrib
 {
     if [ -e /etc/debian_version ] ; then
@@ -79,11 +113,11 @@ function brGetDistrib
     [ "$DISTRIB_ID" != "" -a "$DISTRIB_RELEASE" != "" ] || brError "brGetDistrib failed"
 }
 
-# check if distrib is "Ubuntu" or "Debian"
+##! check if distrib is "Ubuntu" or "Debian"
 function brIsDistrib
 {
-    [ $DISTRIB_ID != "" ] || brGetDistrib
-    if [ $DISTRIB_ID == "$1" ] ; then
+    [ "$DISTRIB_ID" != "" ] || brGetDistrib
+    if [ "$DISTRIB_ID" == "$1" ] ; then
         return 0
     else
         return 1
@@ -115,7 +149,7 @@ function brBold()
 function brDateTime()
 {
     date "$@" '+%Y-%m-%d_%H%M'
-#    date '+%F'
+    #    date '+%F'
 }
 
 # return time
@@ -128,6 +162,12 @@ function brTime()
 function brYear()
 {
     date "$@" '+%Y'
+}
+
+# get current month
+function brMonth()
+{
+    date "$@" '+%m'
 }
 
 ############ LOGGING AND INFO ######################################33
@@ -151,37 +191,44 @@ function brStatusOut()
 {
     let col=$(( $(ansiColumns) - ${#1} - ${#2} + ${#3})) 
 
-#    echo "cols=$(tput cols) #1=${#1} #2=${#2} fill=$col"
+    #    echo "cols=$(tput cols) #1=${#1} #2=${#2} fill=$col"
     printf '%s%*s%s%s%s\n' "$1" "$col" "$3" "$2" "$ansinormal"
 }
 
-function bfFnLn()
+function brFnLn()
 {
-    if [[ "$3" == "-d" ]] ; then
+    if [[ "${3-}" == "-d" ]] ; then
         [ $BRDEBUG != 0 ] && echo "$2:$1"
     else
         echo "$2:$1"
     fi
 }
 
+# notification on desktop
+function brNotify()
+{
+    notify-send "$*"
+    brInfo "$*"
+}
+
 # display info
 function brInfo()
 {
-#    echo "$*"
+    #    echo "$*"
     brStatusOut "$*" "[ INFO  ]" "${ansinormal}"
 }
 
 # display error and exit
 function brWarn()
 {
-    brStatusOut "$*" "$(bfFnLn $(caller) -d) [ WARN  ]" ${ansiyellow}
+    brStatusOut "$*" "$(brFnLn $(caller) -d) [ WARN  ]" ${ansiyellow}
 }
 
 # display debug
 function brDebug()
 {
-#    [ $BRDEBUG == 1 ] && brOutputHook "[${ansicyan}DEBUG${ansinormal}] $* ($(caller))"
-[ $BRDEBUG == 1 ] && brStatusOut "$*" "$(bfFnLn $(caller)) [ DEBUG ]" "${ansicyan}"
+    #    [ $BRDEBUG == 1 ] && brOutputHook "[${ansicyan}DEBUG${ansinormal}] $* ($(caller))"
+    [ $BRDEBUG == 1 ] && brStatusOut "$*" "$(brFnLn $(caller)) [ DEBUG ]" "${ansicyan}"
 }
 
 function brCatchErrors()
@@ -195,11 +242,11 @@ function brCatchErrors()
 # syntax: brTrapWithArg func signals
 # example: 
 function brTrapWithArg() {
-    func="$1" ; shift
-    for sig ; do
-        trap "$func $sig" "$sig"
-        echo setting trap: "$func $sig" for $sig
-    done
+func="$1" ; shift
+for sig ; do
+    trap "$func $sig" "$sig"
+    echo setting trap: "$func $sig" for $sig
+done
 }
 
 # function that can/should be overloaded for cleanup purposes
@@ -236,12 +283,26 @@ function brTrapsOff()
 # display error and exit
 function brError()
 {
-    brStatusOut "$*" "$(bfFnLn $(caller) -d) [ ERROR ]" ${ansilightred}
+    brStatusOut "$*" "$(brFnLn $(caller) -d) [ ERROR ]" ${ansilightred}
     logger "*** ERROR: $* ($(caller))"
     BRERRORCOUNT=$((BRERRORCOUNT + 1 ))
     if [ $BRERRORABORT == 1 ] ; then 
         exit 1
     fi
+}
+
+# echo nonobstrusive alert 
+function brBeep()
+{
+    local SOUND=""
+    local PLAYER=""
+
+    [ -e /usr/share/sounds/pop.wav ] && SOUND=/usr/share/sounds/pop.wav
+    [ -e $HOME/bin/sounds/pop.wav ] && SOUND=$HOME/bin/sounds/pop.wav
+    #    brIsRunnable "aplay" && PLAYER="aplay -q "
+    #    brIsRunnable "paplay" && PLAYER="paplay "
+    PLAYER="paplay "
+    [ -e $SOUND ] && $PLAYER $SOUND
 }
 
 function brErrorCheck()
@@ -252,16 +313,97 @@ function brErrorCheck()
     fi
 }
 
+########### OS UTILITIES ################3
+
+##! add $1 to path if $1 exists and is not in path
+function brAddToPath()
+{
+    if [ -d "$1" ] ; then
+        [[ ":$PATH:" != *":$1:"* ]] && export PATH="$1:${PATH}"
+        #        echo $PATH , $1 added
+    fi
+}
+
+##! kill process and its children
+function brKillProcessTree
+{
+    kill -- -$(ps -o pgid= $PID | grep -o [0-9]*)
+}
+
+##! run process and kill it after timeout
+# $1: timeout in seconds
+# $2*: process to start
+# Ref: https://stackoverflow.com/questions/10028820/bash-wait-with-timeout
+function brTimeoutProcess
+{
+    local pidFile=`mktemp`
+    local timeOut=$1
+    shift
+    ( exec $* ; rm $pidFile ; ) &
+    pid=$!
+    echo $pid > $pidFile
+    ( sleep $timeOut ; if [[ -e $pidFile ]]; then brKillProcessTree $pid ; fi ; ) &
+    killerPid=$!
+    wait $pid
+    kill $killerPid
+    brLazyRm $pidFile
+}
+
+
 ########### FILE Utility functions################3
 
-# return size of file
+##! Get CPU Load
+function brLoad()
+{
+    echo $(cut -d ' ' -f 2 < /proc/loadavg | cut -f 1 -d ".")
+    #uptime | cut -f 14 -d " " | cut -f 1 -d "."
+}
+
+##! Determine current internet speed
+function brNetSpeed()
+# return download speed in Kb/s
+{
+    #    local url="http://garuda.epr.ch/speedtest"
+    #    local url="http://speedtest.pixelwolf.ch"
+    local url="http://www.google.ch"
+    wget $url -O /dev/null 2>&1 | tail -n 2 | awk '{
+    kbps=substr($3,2)
+    if ($4=="MB/s)")
+        kbps*=1024
+        printf("%ld", kbps)
+    }
+    '
+}
+
+##! remove file if it exists
+function brLazyRm()
+{
+    if [ -e "$*" ] ; then
+        rm "$*" || brWarn error removing $*
+    fi
+}
+
+##! make directory if it does not exist
+function brLazyMkDir()
+{
+    [ -d "$*" ] || mkdir -p "$*" || brWarn error removing $*
+}
+
+##! return size of file
 function brFileSize()
 {
     local fs=0$(stat --printf="%s" "$*" 2>/dev/null)
     echo $fs
 }
 
-# check for programs required by a script
+#check if program is available
+#function brIsRunnable()
+#{
+#        which $p > /dev/null && return 0
+#        return 1
+#}
+
+##! check for programs required by a script
 function brRequire()
 {
     local sbre=$BRERRORABORT
@@ -276,7 +418,7 @@ function brRequire()
     BRERRORABORT=$sbre
 }
 
-# run if a file exists, abort with error message if not
+##! run if a file exists, abort with error message if not
 function brRunFile()
 {
     [ -e "$1" ] || brError Running file $1 failed: not found
@@ -285,7 +427,7 @@ function brRunFile()
 
 ############ FORMATTED OUTPUT #################3
 
-# center text and pad it 
+##! center text and pad it 
 function brCenterpad()
 {
     local LEN=$(ansiColumns)
@@ -303,34 +445,35 @@ function brCenterpad()
         local ldon=""
         local ldoff=""
     fi
-        
 
     pad=`printf '%0.1s' "$dash"{1..200}`
     echo -e "${ansibold}${ldon}${pad:1:$topad}$ldoff$*${ldon}${pad:1:$topadd}${ldoff}${ansinormal}"
 }
 
+##! wait for any key before continuing
 function brPressAnyKey
 {
     local prompt="${ansibold}Press any key to continue${ansinormal}"
-    printf $prompt
-    read -n -1 -r
+    printf "$prompt"
+    read -n 1 -r
     echo
 }
 
+##! request yes or no answer. First parameter may be -Y or -N default
 function brYesNo
 {
     local prompt
     local default
     if [ "${1:-}" = "-Y" ]; then
-        prompt="${ansibold}Y${ansinormal}n"
+        prompt="${ansibold}Y/${ansinormal}n"
         default=Y
         shift
     elif [ "${1:-}" = "-N" ]; then
-        prompt="y${ansibold}N${ansinormal}"
+        prompt="y${ansibold}N/${ansinormal}"
         default=N
         shift
     else
-        prompt="yn"
+        prompt="y/n"
         default=
     fi
     while true; do
@@ -348,25 +491,63 @@ function brYesNo
         esac
         echo ?
     done    
-#    local REPLY="?"
-#    local default="?"
-#    while [[ ! $REPLY =~ ^[YyJjNn]$ ]] ; do
-#        read -p "$* [yn]?" -n 1 -r
-#        echo
-#    done
-#    if [[ $REPLY =~ ^[YyJj]$ ]] ; then
-#        exit 0
-#    else
-#        exit 1
-#    fi
+    #    local REPLY="?"
+    #    local default="?"
+    #    while [[ ! $REPLY =~ ^[YyJjNn]$ ]] ; do
+    #        read -p "$* [yn]?" -n 1 -r
+    #        echo
+    #    done
+    #    if [[ $REPLY =~ ^[YyJj]$ ]] ; then
+    #        exit 0
+    #    else
+    #        exit 1
+    #    fi
 }
 
-fi
 #echo -e brVersion=$brVersion ${ansired}TERM=$TERM${ansinormal} $(ansiColumns)
 
-if [ "$1" == "-BRDEBUG" ] ; then
+# Parse yaml files
+# https://gist.github.com/pkuczynski/8665367
+# Sample file: zconfig.yml
+#development:
+#  adapter: mysql2
+#  encoding: utf8
+#  database: my_database
+#  username: root
+#  password:
+#Testing
+#!/bin/sh
+## include parse_yaml function
+#. brlib.sh
+#
+## read yaml file
+#eval $(brParseYaml zconfig.yml "config_")
+#
+## access yaml content
+#echo $config_development_database
+
+function brParseYaml() {
+local prefix=$2
+local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+    -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+awk -F$fs '{
+indent = length($1)/2;
+vname[indent] = $2;
+for (i in vname) {if (i > indent) {delete vname[i]}}
+    if (length($3) > 0) {
+        vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+        printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+    }
+}'
+}
+
+if [ "${1-}" == "-BRDEBUG" ] ; then
     BRDEBUG=1
     brDebug "Debug mode enabled"
     shift
 fi
+
+#[ -e $HOME/.config/Xdbus ] && source $HOME/.config/Xdbus
+
 
